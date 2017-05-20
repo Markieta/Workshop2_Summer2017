a52d66632094cb1fc097ae00be6ef8009e359785
Christopher Markieta <markietachristopher@gmail.com>
2017-05-20 15:55:27 -0400

    Removing verbose, unnecessary comments.

--- app/src/main/java/com/markieta/workshop2_summer2017/MainActivity.java
+++ app/src/main/java/com/markieta/workshop2_summer2017/MainActivity.java
@@ -12,26 +12,17 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    //handle the button 2 click
     private void a2_button_click_handler(){
-        //display a toast for debugging - show the user...
         Toast.makeText(this, "a2 button was clicked", Toast.LENGTH_LONG).show();
-        //use an intent to launch our SecondActivity
-        //1. create an Intent (in this case implicit)
+
         Intent a2Intent = new Intent("com.seneca.lab2b.cmarkieta");
-        //2. use the Intent to start the SecondActivity
-        //startActivityForResult expects both an Intent and a request code so that you can
-        //match up the request with an eventual reply
         startActivityForResult(a2Intent, 1);
     }
 
     private void a3_button_click_handler(){
-        //display a toast for debugging - show the user...
         Toast.makeText(this, "a3 button was clicked", Toast.LENGTH_LONG).show();
-        //use an explicit intent to launch our ThirdActivity
-        //1. create an Intent (in this case explicit)
+
         Intent a3Intent = new Intent(this, ThirdActivity.class);
-        //2. use the Intent to start the ThirdActivity
         startActivity(a3Intent);
     }
 
@@ -40,10 +31,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //add code to the button handler for a2_button
-        //get a reference to the button
         Button a2_button = (Button) findViewById(R.id.a2_button);
-        //set an onclickhandler for the button
+
         a2_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -51,9 +40,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //set a button handler for a3_button
         Button a3_button = (Button) findViewById(R.id.a3_button);
-        //set an onclickhandler for the button
+
         a3_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -62,23 +50,13 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    /*onActivityResult will be called by the Android framework once the activity we started
-    with startActivityForResult completes. We are passed the same request code that we passed to
-    startActivityForResult and a resultcode that indicates whether the user completed the ACtivity
-    or pressed Back to cancel
-     */
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        //handler
         if (requestCode == 1){
-            //check to see whether the user pressed OK or hit the back button
             if(resultCode == Activity.RESULT_OK){
-                //extract the message from the Intent that was returned (data)
                 String result = data.getStringExtra("message");
-                //set the textView's string to display the message
                 TextView text = (TextView) findViewById(R.id.textView1);
                 text.setText(result);
             }
--- app/src/main/java/com/markieta/workshop2_summer2017/SecondActivity.java
+++ app/src/main/java/com/markieta/workshop2_summer2017/SecondActivity.java
@@ -11,19 +11,12 @@ import android.widget.EditText;
 public class SecondActivity extends AppCompatActivity {
 
     private void ok_button_click_handler(){
-        //compose a reply to send back to MainActivity
-        //use an Intent to store the reply
-        Intent resultIntent = new Intent(); //empty for now
+        Intent resultIntent = new Intent();
 
-        //get the text that the user entered into the text box
         EditText editText = (EditText) findViewById(R.id.editText);
-        //extract the text from the editText
         String message = editText.getText().toString();
-        //add an extra data to the Intent
         resultIntent.putExtra("message", message);
-        //this next line is only needed if you are returning a result to the calling activity
         setResult(Activity.RESULT_OK, resultIntent);
-        //end SecondActivity
         finish();
     }
 
@@ -33,7 +26,6 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
 
         Button ok_button = (Button) findViewById(R.id.ok_button);
-        //set an onclickhandler for the button
         ok_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
